3d46a677135b69e1e3e407e093e4fb1d92a72975
Smeky <smeky.sobota@gmail.com>
2016-01-23 20:11:49 +0100

    Color implementation

---
+++ src/mgfw/graphics/Color.cpp
@@ -0,0 +1,112 @@
+#include "Color.h"
+
+#include <algorithm>
+
+namespace mg
+{
+
+const Color Color::Transparent = Color(255, 255, 255, 0);
+const Color Color::White       = Color(255, 255, 255 );
+const Color Color::Black       = Color(0, 0, 0);
+
+const Color Color::Red         = Color(255, 0, 0);
+const Color Color::Green       = Color(0, 255, 0);
+const Color Color::Blue        = Color(0, 0, 255);
+const Color Color::Yellow      = Color(255, 255, 0);
+const Color Color::Orange      = Color(255, 153, 0);
+const Color Color::Purple      = Color(255, 0, 255);
+
+Color::Color()
+: Color(0, 0, 0)
+{}
+
+Color::Color(uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _a /* = 255 */)
+: r(_r)
+, g(_g)
+, b(_b)
+, a(_a)
+{}
+
+std::array<float, 4> Color::getNormalized() const
+{
+    std::array<float, 4> normalized = {
+        (float)(r) / 255,
+        (float)(g) / 255,
+        (float)(b) / 255,
+        (float)(a) / 255,
+    };
+
+    return normalized;
+}
+
+uint8_t& Color::operator[](uint8_t index)
+{
+    return data[ index ];
+}
+
+const uint8_t& Color::operator[](uint8_t index) const
+{
+    return data[ index ];
+}
+
+bool operator==(const Color& left, const Color& right)
+{
+    return (left.r == right.r) &&
+           (left.g == right.b) &&
+           (left.b == right.b) &&
+           (left.a == right.a);
+}
+
+bool operator!=(const Color& left, const Color& right)
+{
+    return !(left == right);
+}
+
+Color operator+(const Color& left, const Color& right)
+{
+    return Color(std::min(left.r + right.r, 255),
+                  std::min(left.g + right.g, 255),
+                  std::min(left.b + right.b, 255),
+                  std::min(left.a + right.a, 255));
+}
+
+Color operator-(const Color& left, const Color& right)
+{
+    return Color(std::max((int8_t)left.r - right.r, 0),
+                  std::max((int8_t)left.g - right.g, 0),
+                  std::max((int8_t)left.b - right.b, 0),
+                  std::max((int8_t)left.a - right.a, 0));
+}
+
+Color operator*(const Color& left, const Color& right)
+{
+    return Color((int)left.r * right.r / 255,
+                  (int)left.g * right.g / 255,
+                  (int)left.b * right.b / 255,
+                  (int)left.a * right.a / 255);
+}
+
+Color operator+=(Color& left, const Color& right)
+{
+    return left = left + right;
+}
+
+Color operator-=(Color& left, const Color& right)
+{
+    return left = left - right;
+}
+
+Color operator*=(Color& left, const Color& right)
+{
+    return left = left * right;
+}
+
+std::ostream& operator<<(std::ostream& os, const Color& color)
+{
+    return os << "Color: r=" << (int)color.r
+              << " g=" << (int)color.g
+              << " b=" << (int)color.b
+              << " a=" << (int)color.a;
+}
+
+} // namespace mg
---
+++ src/mgfw/graphics/Color.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#ifdef MG_GCC
+#include <stdint-gcc.h>
+#else
+#include <stdint.h>
+#endif // MG_GCC
+
+#include <array>
+#include <iostream>
+
+namespace mg
+{
+
+struct Color
+{
+    union
+    {
+        uint8_t data[4];
+        struct{ uint8_t r, g, b, a; };
+    };
+
+    Color();
+    Color(uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _a = 255);
+
+    uint8_t& operator[](uint8_t index);
+    const uint8_t& operator[](uint8_t index) const;
+
+    std::array<float, 4> getNormalized() const;
+
+    static const Color Transparent;
+    static const Color White;
+    static const Color Black;
+
+    static const Color Red;
+    static const Color Green;
+    static const Color Blue;
+    static const Color Yellow;
+    static const Color Orange;
+    static const Color Purple;
+};
+
+bool operator==(const Color& left, const Color& right);
+bool operator!=(const Color& left, const Color& right);
+Color operator+(const Color& left, const Color& right);
+Color operator-(const Color& left, const Color& right);
+Color operator*(const Color& left, const Color& right);
+Color operator+=(Color& left, const Color& right);
+Color operator-=(Color& left, const Color& right);
+Color operator*=(Color& left, const Color& right);
+std::ostream& operator<<(std::ostream& os, const Color& color);
+
+} // namespace mg
