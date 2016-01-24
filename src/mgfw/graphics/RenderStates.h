5baf263067ef65d617c09947290e5ce123e527b5
Smeky <smeky.sobota@gmail.com>
2016-01-24 20:13:35 +0100

    Fixed wrong use of const

--- src/mgfw/graphics/RenderStates.h
+++ src/mgfw/graphics/RenderStates.h
@@ -8,7 +8,7 @@ class ShaderProgram;
 // TODO(Smeky) Add transform and texture once they are implemented
 struct RenderStates
 {
-    const ShaderProgram* shader = nullptr;
+    ShaderProgram* shader = nullptr;
 };
 
 } // namespace mg
