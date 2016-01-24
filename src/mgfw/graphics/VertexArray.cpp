3475f7323ae5421b9fe35a00a72636b2be3964de
Smeky <smeky.sobota@gmail.com>
2016-01-24 20:08:08 +0100

    VertexArray impl

---
+++ src/mgfw/graphics/VertexArray.cpp
@@ -0,0 +1,44 @@
+#include "VertexArray.h"
+
+namespace mg
+{
+
+VertexArray::VertexArray()
+: type(PrimitiveType::PType_Triangles)
+{}
+
+VertexArray::VertexArray(PrimitiveType _type, uint32_t size /* = */)
+: type(_type)
+{
+    if(size != 0)
+    {
+        vertices.resize(size);
+    }
+}
+
+Vertex& VertexArray::operator[](uint32_t index)
+{
+    return vertices[index];
+}
+
+const Vertex& VertexArray::operator[](uint32_t index) const
+{
+    return vertices[index];
+}
+
+void VertexArray::resize(uint32_t size)
+{
+    vertices.resize(size);
+}
+
+size_t VertexArray::size() const
+{
+    return vertices.size();
+}
+
+const Vertex* VertexArray::data() const
+{
+    return vertices.data();
+}
+
+} // namespace mg
---
+++ src/mgfw/graphics/VertexArray.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+
+#include "../graphics/Vertex.h"
+#include "../graphics/PrimitiveType.h"
+
+namespace mg
+{
+
+struct VertexArray
+{
+    VertexArray();
+    VertexArray(PrimitiveType type, uint32_t size = 0);
+
+    Vertex& operator[](uint32_t index);
+    const Vertex& operator[](uint32_t index) const;
+
+    void resize(uint32_t size);
+    size_t size() const;
+    const Vertex* data() const;
+
+    std::vector<Vertex> vertices;
+    PrimitiveType type;
+};
+
+} // namespace mg
