029ebb9471d74b775f30dcf8e4868bd222334e51
Smeky <smeky.sobota@gmail.com>
2016-01-24 20:01:23 +0100

    Primitive Type impl

---
+++ src/mgfw/graphics/PrimitiveType.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace mg
+{
+
+enum PrimitiveType {
+    PType_Points,
+    PType_Lines,
+    PType_LineLoop,
+    PType_LineStrip,
+    PType_Triangles,
+    PType_TriangleStrip,
+    PType_TriangleFan,
+    PType_Quads,
+    PType_QuadStrip,
+    PType_Polygon,
+};
+
+} // namespace mg
