0da43f1a9262e438dd89d3b5ba5193c45922bfe9
Smeky <smeky.sobota@gmail.com>
2016-02-08 22:41:42 +0100

    Font file - default loading

--- src/mgfw/graphics/Font.cpp
+++ src/mgfw/graphics/Font.cpp
@@ -1,10 +1,42 @@
 #include "Font.h"
 
+#include "SDL2/SDL_ttf.h"
+
 #include "../system/ErrorLog.h"
 
 namespace mg {
 
 Font::Font()
+: m_handle(nullptr)
 {}
+
+Font::~Font() {
+    clear();
+}
+
+bool Font::loadFromFile(const std::string& filename) {
+    clear();
+
+    m_handle = TTF_OpenFont(filename.c_str(), 11);
+
+    if(!m_handle) {
+        priv::logError(
+                "Failed to open font at: " +
+                filename +
+                "\n" +
+                TTF_GetError()
+        );
+
+        return false;
+    }
+
+    return true;
+}
+
+void Font::clear() {
+    if(m_handle) {
+        TTF_CloseFont((TTF_Font*)m_handle);
+    }
+}
 
 } // namespace mg
--- src/mgfw/graphics/Font.h
+++ src/mgfw/graphics/Font.h
@@ -1,12 +1,21 @@
 #pragma once
 
+#include <string>
+
 namespace mg {
 
 class Font {
 public:
     Font();
+    ~Font();
+
+    bool loadFromFile(const std::string& filename);
 
 private:
+    void clear();
+
+private:
+    void* m_handle;
 
 };
 
