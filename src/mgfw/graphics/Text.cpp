d6dae63eaa28039c629b0223879b29a29bd50b1e
Smeky <smeky.sobota@gmail.com>
2016-02-08 22:49:47 +0100

    Default get/set methods

--- src/mgfw/graphics/Text.cpp
+++ src/mgfw/graphics/Text.cpp
@@ -5,6 +5,31 @@
 namespace mg {
 
 Text::Text()
+: m_font(nullptr)
 {}
+
+void Text::setFont(const Font* font) {
+    m_font = font;
+}
+
+const Font* Text::getFont() const {
+    return m_font;
+}
+
+void Text::setString(const std::string& str) {
+    m_string = str;
+}
+
+const std::string& Text::getString() const {
+    return m_string;
+}
+
+void Text::setCharSize(uint16_t charSize) {
+    m_charSize = charSize;
+}
+
+uint16_t Text::getCharSize() const {
+    return m_charSize;
+}
 
 } // namespace mg
--- src/mgfw/graphics/Text.h
+++ src/mgfw/graphics/Text.h
@@ -13,8 +13,20 @@ class Text {
 public:
     Text();
 
-private:
+    void setFont(const Font* font);
+    const Font* getFont() const;
 
+    void setString(const std::string& str);
+    const std::string& getString() const;
+
+    void setCharSize(uint16_t charSize);
+    uint16_t getCharSize() const;
+
+private:
+    const Font* m_font;
+
+    std::string m_string;
+    uint16_t m_charSize;
 };
 
 } // namespace mg
